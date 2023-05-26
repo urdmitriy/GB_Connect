da7d9166f5705fb4c16f7387dd410c58b081f162
Dmitriy Uryupin <urdmitriy@yandex.ru>
2023-05-26 12:51:00 +0300

    HW 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,90 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
 
-const char wifi_ssid[] = {"MTS_GPON_3BF6"};
-const char wifi_pass[] = {"CdARCCTH"};
-const char address_page[] = {"https://jsonplaceholder.typicode.com/posts/1"};
+#define SERVICE_BATTERY BLEUUID((uint16_t)0x180F)
+#define SERVICE_LEDLIGHT BLEUUID((uint16_t)0x1815)
+
+BLECharacteristic battLevelCharact(BLEUUID((uint16_t) 0x2A19),BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic ledLightCharact(BLEUUID((uint16_t) 0x2A3F), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+BLEDescriptor battLevelDescriptor(BLEUUID((uint16_t)0x2901));
+BLEDescriptor ledLightDescriptor(BLEUUID((uint16_t)0x2901));
+
+class LEDCallBacks : public BLECharacteristicCallbacks
+{
+private:
+  /* data */
+public:
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string value = pCharacteristic->getValue();
+
+    if (value == "1"){
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    Serial.print("Write data ");
+
+  }
+};
+
+
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-  WiFi.begin(wifi_ssid, wifi_pass);
-  Serial.print("Connecting");
-  while (WiFi.status() != wl_status_t::WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("Ok");
-  Serial.println("Ip address: " + WiFi.localIP().toString());
-  Serial.println("MAC address: " + WiFi.macAddress());
+  Serial.println("Starting service");
 
-  HTTPClient client;
-  client.begin(address_page);
-  Serial.printf("Address page: %s\n\r", address_page);
-  int response_code = client.GET();
-  if (response_code == HTTP_CODE_OK){
-    Serial.printf("Response code: %d\n\r", response_code);
-    Serial.println("Data:");
-    Serial.println(client.getString());
-  }
-  else{
-    Serial.printf("Response code: %d\n\r", response_code);
-  }
+  BLEDevice::init("GB IoT device");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *battService = pServer->createService(SERVICE_BATTERY);
+  BLEService *ledLightService = pServer->createService(SERVICE_LEDLIGHT);
+  
+  battService->addCharacteristic(&battLevelCharact);
+  battLevelCharact.addDescriptor(new BLE2902());
+
+  ledLightService->addCharacteristic(&ledLightCharact);
+  ledLightCharact.addDescriptor(new BLE2902());
+
+  battLevelDescriptor.setValue("Level from 0 to 100");
+  battLevelCharact.addDescriptor(&battLevelDescriptor);
+
+  ledLightDescriptor.setValue("1 - ON, 0 - OFF");
+  ledLightCharact.addDescriptor(&ledLightDescriptor);
+  ledLightCharact.setCallbacks(new LEDCallBacks());
+
+  battService->start();
+  ledLightService->start();
+
+  BLEAdvertising *battLevelAdv = BLEDevice::getAdvertising();
+  battLevelAdv->addServiceUUID(SERVICE_BATTERY);
+  battLevelAdv->setScanResponse(true);
+  battLevelAdv->start();
+
+  BLEAdvertising *ledLightAdv = BLEDevice::getAdvertising();
+  ledLightAdv->addServiceUUID(SERVICE_LEDLIGHT);
+  ledLightAdv->setScanResponse(true);
+  ledLightAdv->start();
 }
 
+uint8_t level = 12;
+
+
 void loop() {
+  battLevelCharact.setValue(&level,1);
+  battLevelCharact.notify();
+  delay(1000);
+  level++;
+  if ((int)level > 100){
+    level = 0;
+  }
+  // Serial.printf("Level: %d\n\r", level);
+  
   
 }
